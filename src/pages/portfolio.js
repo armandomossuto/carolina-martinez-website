2b77dc1df169ae132d220e6773d8c3b6a13c4e92
Armando Mossuto <armandomossuto@MacBook-Pro-Armando-Mossuto.local>
2019-04-23 16:56:38 +0100

    feature: creating main routes

---
+++ src/pages/portfolio.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const PortfolioPage = () => (
+  <Layout>
+    <SEO title="About" keywords={[`portfolio carolina martinez`, `portfolio video-edito`, `portfolio`]} />
+    <h1>Portfolio</h1>
+  </Layout>
+)
+
+export default PortfolioPage

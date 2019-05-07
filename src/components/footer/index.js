ee0d1c1c9280f7e3f01bb4895f8b6f36db00b854
Armando Mossuto <armandomossuto@MacBook-Pro-Armando-Mossuto.local>
2019-05-07 17:34:11 +0100

    feature: footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,8 +1,16 @@
 import React from "react"
+import linkedinLogo from "../../images/linkedin.png"
+import youtubeLogo from "../../images/youtube.png"
 
 const Footer = () =>
-  <footer class="footer">
-    © {new Date().getFullYear()}, Carolina Martinez
+  <footer className="footer">
+    <span className="footer__date">
+      {new Date().toDateString()} Lisbon, Pt
+    </span>
+    <span className="footer__logos">
+      <a href="/#"><img src={linkedinLogo} alt="Linkdin Logo"></img></a>
+      <a href="/#"><img  src={youtubeLogo} alt="Youtube Logo"></img></a>
+    </span>
   </footer>
 
 export default Footer

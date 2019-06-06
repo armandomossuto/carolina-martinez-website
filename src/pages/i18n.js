d8f1ba38b4daf6d647427aa7c0d1eab5d8e8be03
Armando Mossuto <armandomossuto@MacBook-Pro-Armando-Mossuto.local>
2019-06-06 19:49:00 +0100

    feature: about/contact messages

--- src/pages/i18n.js
+++ src/pages/i18n.js
@@ -51,6 +51,19 @@ i18n
             notification: {
               error: 'Something went wrong with your request. Please try again and if it keeps happening, do not hesitate to use the contact infotmation displayed bellow.',
               success: 'Your message has been sent. I will contact you as soon as possible.'
+            },
+          },
+          welcome: {
+            message: {
+              1: 'I am Carolina Martínez, a professional video editor, currently working in Lisbon, Portugal.',
+              2: 'I have over seven years of experience in diferent proyects : TV shows, video clips, events, weddings, holidays, Interviews, corporate films, product films, documentaries and promotionals.',
+              3: 'For any inquiry, please contact me. I am at your service.'
+            }
+          },
+          about: {
+            message: {
+              1: 'I am a publicist from Venezuela, with studies in film and television. Worked six years editing for a tv producer and along the way I started independent works.',
+              2: 'I love transforming footage into an actual film. Let me invite you to see my portafolio so you can get to know my work better.'
             }
           }
         },
@@ -88,6 +101,19 @@ i18n
             notification: {
               error: 'Ocurrió un error con su solicitud. Port favor, vuelva a intentar y si el error persiste no dude en utilizar los datos de contacto que se encuentran en la parte inferior de esta página.',
               success: 'Su mensaje se envió correctamente. Le daré una respuesta ran rápido como sea posible.'
+            },
+          },
+          welcome: {
+            message: {
+              1: 'Mi nombre es Carolina Martínez y soy una editora de video profesional que reside en Lisboa, Portugal.',
+              2: 'Tengo más de siete años de experiencia en diferentes tipos de proyectos: programas de TV, video clips, eventos, matrimonios, vacaciones, entrevistas, films corporativos, documentales y videos promocionales.',
+              3: 'No dude en contactarme. Estoy a su servicio.'
+            }
+          },
+          about: {
+            message: {
+              1: 'Tengo un título en publicidad de Venezuela, y estudios en cine y televisión. Trabajé seis años para un productor de TV junto a la realización de trabajos independientes.',
+              2: 'Me encanta transformar el material en un film. Le invito a ver mi portafolio para que conozca mi trabajo mejor'
             }
           }
         },
@@ -125,6 +151,19 @@ i18n
             notification: {
               error: 'TODO Ocurrió un error con su solicitud. Port favor, vuelva a intentar y si el error persiste no dude en utilizar los datos de contacto que se encuentran en la parte inferior de esta página.',
               success: 'TODO Su mensaje se envió correctamente. Le daré una respuesta ran rápido como sea posible.'
+            },
+          },
+          welcome: {
+            message: {
+              1: 'TODO Mi nombre es Carolina Martínez y soy una editora de video profesional que reside en Lisboa, Portugal.',
+              2: 'TODO Tengo más de 7 años de experiencia en diferentes tipos de proyectos: programas de TV, video clips, eventos, matrimoios, vacaiones, entrevistas, fils corporativos, documentales y videos promocionales.',
+              3: 'TODO No dude en contactarme. Estoy a su servicio.'
+            }
+          },
+          about: {
+            message: {
+              1: 'TODO Tengo un título en publicidad de Venezuela, y estudios en cine y televisión. Trabajé seis años para un productor de TV junto a la realización de trabajos independientes.',
+              2: 'TODO Me encanta transformar el material en un film. Le invito a ver mi portafolio para que conozca mi trabajo mejor'
             }
           }
         },
